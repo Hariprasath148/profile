1da7865ce144016f10e1aa18344094cfecadd88d
Hariprasath148 <janhharigithub@gmail.com>
2025-05-07 00:22:56 +0530

    project section 50%

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,11 +5,142 @@ import "../style/project.css"
 export const Projects = () => {
   return (
     <>
-      <div className="row">
-        <div className="col-lg-6 col-12 project-discription"></div>
+      <div className="row project-section">
+        <div className="col-lg-6 col-12 project-discription p-lg-5 p-4 d-flex flex-column justify-content-center">
+          <p className='display-6 mb-1 project-name'>Digital Planner</p>
+          <p className='project-discription-para'>Developed a responsive web app using HTML, CSS, and JavaScript for managing to-do lists and weekly plans. Features include customizable themes, layouts, a food list, and a quickaccess menu, ensuring seamless use across devices.</p>
+          <div className='project-tech-stack d-flex flex-wrap gap-2 mb-3'>
+            <p className='px-3 py-2 rounded-2 project-tech-tag m-0'>HTML</p>
+            <p className='px-3 py-2 rounded-2 project-tech-tag m-0'>HTML</p>
+            <p className='px-3 py-2 rounded-2 project-tech-tag m-0'>HTML</p>
+            <p className='px-3 py-2 rounded-2 project-tech-tag m-0'>HTML</p>
+          </div>
+          <div className='project-button-container mt-2'>
+            <a href="#" className='px-3 py-2 me-3 text-decoration-none rounded-2 project-navigate-buttons project-navigate-buttons-visit'>Visit Site</a>
+            <a href="#" className='px-3 py-2 text-decoration-none rounded-2 project-navigate-buttons project-navigate-buttons-code'>View Code</a>
+          </div>
+        </div>
         <div className="col-lg-6 col-12 position-relative project-photo d-flex align-items-center justify-content-center">
             <img src={pic} alt="" className='project-img' />
             <div className="position-absolute w-100 h-100 project-img-shadhow"></div>
+            <div className="position-absolute project-img-center-line"></div>
+            <div className="position-absolute project-img-side-line"></div>
+            <div className="position-absolute project-img-side-Diamond"></div>
+        </div>
+      </div>
+      <div className="row project-section">
+        <div className="col-lg-6 col-12 project-discription p-lg-5 p-3 d-flex flex-column justify-content-center">
+          <p className='display-6 mb-1 project-name'>Digital Planner</p>
+          <p className='project-discription-para'>Developed a responsive web app using HTML, CSS, and JavaScript for managing to-do lists and weekly plans. Features include customizable themes, layouts, a food list, and a quickaccess menu, ensuring seamless use across devices.</p>
+          <div className='project-tech-stack d-flex flex-wrap gap-2 mb-3'>
+            <p className='px-3 py-2 rounded-2 project-tech-tag m-0'>HTML</p>
+            <p className='px-3 py-2 rounded-2 project-tech-tag m-0'>HTML</p>
+            <p className='px-3 py-2 rounded-2 project-tech-tag m-0'>HTML</p>
+            <p className='px-3 py-2 rounded-2 project-tech-tag m-0'>HTML</p>
+          </div>
+          <div className='project-button-container mt-2'>
+            <a href="#" className='px-3 py-2 me-3 text-decoration-none rounded-2 project-navigate-buttons project-navigate-buttons-visit'>Visit Site</a>
+            <a href="#" className='px-3 py-2 text-decoration-none rounded-2 project-navigate-buttons project-navigate-buttons-code'>View Code</a>
+          </div>
+        </div>
+        <div className="col-lg-6 col-12 position-relative project-photo d-flex align-items-center justify-content-center">
+            <img src={pic} alt="" className='project-img' />
+            <div className="position-absolute w-100 h-100 project-img-shadhow"></div>
+            <div className="position-absolute project-img-center-line"></div>
+            <div className="position-absolute project-img-side-line"></div>
+            <div className="position-absolute project-img-side-Diamond"></div>
+        </div>
+      </div>
+      <div className="row project-section">
+        <div className="col-lg-6 col-12 project-discription p-lg-5 p-3 d-flex flex-column justify-content-center">
+          <p className='display-6 mb-1 project-name'>Digital Planner</p>
+          <p className='project-discription-para'>Developed a responsive web app using HTML, CSS, and JavaScript for managing to-do lists and weekly plans. Features include customizable themes, layouts, a food list, and a quickaccess menu, ensuring seamless use across devices.</p>
+          <div className='project-tech-stack d-flex flex-wrap gap-2 mb-3'>
+            <p className='px-3 py-2 rounded-2 project-tech-tag m-0'>HTML</p>
+            <p className='px-3 py-2 rounded-2 project-tech-tag m-0'>HTML</p>
+            <p className='px-3 py-2 rounded-2 project-tech-tag m-0'>HTML</p>
+            <p className='px-3 py-2 rounded-2 project-tech-tag m-0'>HTML</p>
+          </div>
+          <div className='project-button-container mt-2'>
+            <a href="#" className='px-3 py-2 me-3 text-decoration-none rounded-2 project-navigate-buttons project-navigate-buttons-visit'>Visit Site</a>
+            <a href="#" className='px-3 py-2 text-decoration-none rounded-2 project-navigate-buttons project-navigate-buttons-code'>View Code</a>
+          </div>
+        </div>
+        <div className="col-lg-6 col-12 position-relative project-photo d-flex align-items-center justify-content-center">
+            <img src={pic} alt="" className='project-img' />
+            <div className="position-absolute w-100 h-100 project-img-shadhow"></div>
+            <div className="position-absolute project-img-center-line"></div>
+            <div className="position-absolute project-img-side-line"></div>
+            <div className="position-absolute project-img-side-Diamond"></div>
+        </div>
+      </div>
+      <div className="row project-section">
+        <div className="col-lg-6 col-12 project-discription p-lg-5 p-3 d-flex flex-column justify-content-center">
+          <p className='display-6 mb-1 project-name'>Digital Planner</p>
+          <p className='project-discription-para'>Developed a responsive web app using HTML, CSS, and JavaScript for managing to-do lists and weekly plans. Features include customizable themes, layouts, a food list, and a quickaccess menu, ensuring seamless use across devices.</p>
+          <div className='project-tech-stack d-flex flex-wrap gap-2 mb-3'>
+            <p className='px-3 py-2 rounded-2 project-tech-tag m-0'>HTML</p>
+            <p className='px-3 py-2 rounded-2 project-tech-tag m-0'>HTML</p>
+            <p className='px-3 py-2 rounded-2 project-tech-tag m-0'>HTML</p>
+            <p className='px-3 py-2 rounded-2 project-tech-tag m-0'>HTML</p>
+          </div>
+          <div className='project-button-container mt-2'>
+            <a href="#" className='px-3 py-2 me-3 text-decoration-none rounded-2 project-navigate-buttons project-navigate-buttons-visit'>Visit Site</a>
+            <a href="#" className='px-3 py-2 text-decoration-none rounded-2 project-navigate-buttons project-navigate-buttons-code'>View Code</a>
+          </div>
+        </div>
+        <div className="col-lg-6 col-12 position-relative project-photo d-flex align-items-center justify-content-center">
+            <img src={pic} alt="" className='project-img' />
+            <div className="position-absolute w-100 h-100 project-img-shadhow"></div>
+            <div className="position-absolute project-img-center-line"></div>
+            <div className="position-absolute project-img-side-line"></div>
+            <div className="position-absolute project-img-side-Diamond"></div>
+        </div>
+      </div>
+      <div className="row project-section">
+        <div className="col-lg-6 col-12 project-discription p-lg-5 p-3 d-flex flex-column justify-content-center">
+          <p className='display-6 mb-1 project-name'>Digital Planner</p>
+          <p className='project-discription-para'>Developed a responsive web app using HTML, CSS, and JavaScript for managing to-do lists and weekly plans. Features include customizable themes, layouts, a food list, and a quickaccess menu, ensuring seamless use across devices.</p>
+          <div className='project-tech-stack d-flex flex-wrap gap-2 mb-3'>
+            <p className='px-3 py-2 rounded-2 project-tech-tag m-0'>HTML</p>
+            <p className='px-3 py-2 rounded-2 project-tech-tag m-0'>HTML</p>
+            <p className='px-3 py-2 rounded-2 project-tech-tag m-0'>HTML</p>
+            <p className='px-3 py-2 rounded-2 project-tech-tag m-0'>HTML</p>
+          </div>
+          <div className='project-button-container mt-2'>
+            <a href="#" className='px-3 py-2 me-3 text-decoration-none rounded-2 project-navigate-buttons project-navigate-buttons-visit'>Visit Site</a>
+            <a href="#" className='px-3 py-2 text-decoration-none rounded-2 project-navigate-buttons project-navigate-buttons-code'>View Code</a>
+          </div>
+        </div>
+        <div className="col-lg-6 col-12 position-relative project-photo d-flex align-items-center justify-content-center">
+            <img src={pic} alt="" className='project-img' />
+            <div className="position-absolute w-100 h-100 project-img-shadhow"></div>
+            <div className="position-absolute project-img-center-line"></div>
+            <div className="position-absolute project-img-side-line"></div>
+            <div className="position-absolute project-img-side-Diamond"></div>
+        </div>
+      </div>
+      <div className="row project-section">
+        <div className="col-lg-6 col-12 project-discription p-lg-5 p-3 d-flex flex-column justify-content-center">
+          <p className='display-6 mb-1 project-name'>Digital Planner</p>
+          <p className='project-discription-para'>Developed a responsive web app using HTML, CSS, and JavaScript for managing to-do lists and weekly plans. Features include customizable themes, layouts, a food list, and a quickaccess menu, ensuring seamless use across devices.</p>
+          <div className='project-tech-stack d-flex flex-wrap gap-2 mb-3'>
+            <p className='px-3 py-2 rounded-2 project-tech-tag m-0'>HTML</p>
+            <p className='px-3 py-2 rounded-2 project-tech-tag m-0'>HTML</p>
+            <p className='px-3 py-2 rounded-2 project-tech-tag m-0'>HTML</p>
+            <p className='px-3 py-2 rounded-2 project-tech-tag m-0'>HTML</p>
+          </div>
+          <div className='project-button-container mt-2'>
+            <a href="#" className='px-3 py-2 me-3 text-decoration-none rounded-2 project-navigate-buttons project-navigate-buttons-visit'>Visit Site</a>
+            <a href="#" className='px-3 py-2 text-decoration-none rounded-2 project-navigate-buttons project-navigate-buttons-code'>View Code</a>
+          </div>
+        </div>
+        <div className="col-lg-6 col-12 position-relative project-photo d-flex align-items-center justify-content-center">
+            <img src={pic} alt="" className='project-img' />
+            <div className="position-absolute w-100 h-100 project-img-shadhow"></div>
+            <div className="position-absolute project-img-center-line"></div>
+            <div className="position-absolute project-img-side-line"></div>
+            <div className="position-absolute project-img-side-Diamond"></div>
         </div>
       </div>
     </>
