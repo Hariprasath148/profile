9f63742f05da83f0fa8c4d47e57750689c091145
Hariprasath148 <janhharigithub@gmail.com>
2025-05-06 18:03:31 +0530

    project section process

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import pic from "../assets/project/1.png"
+import "../style/project.css"
 
 export const Projects = () => {
   return (
-    <div>Projects</div>
+    <>
+      <div className="row">
+        <div className="col-lg-6 col-12 project-discription"></div>
+        <div className="col-lg-6 col-12 position-relative project-photo d-flex align-items-center justify-content-center">
+            <img src={pic} alt="" className='project-img' />
+            <div className="position-absolute w-100 h-100 project-img-shadhow"></div>
+        </div>
+      </div>
+    </>
   )
 }
