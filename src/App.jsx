de43b8f373cfc7f8086ad7861b9c861e00053b68
Hariprasath148 <janhharigithub@gmail.com>
2025-05-07 18:19:19 +0530

    project

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
         <Route path="/projects/:id" element={<ProjectsMore/>}/>
         <Route path="/about" element={<About/>}/>
         <Route path="/contact" element={<Contact/>}/>
+        <Route path="*" element={<Contact/>}/>
       </Route>
     )
   )
--- src/pages/ProjectsMore.jsx
+++ src/pages/ProjectsMore.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { Navigate, useParams } from 'react-router-dom'
+import { projects } from '../objects/project';
 
 export const ProjectsMore = () => {
     const params = useParams();
+    const project_presents = projects.some(project => project.route === params.id);
+    const project = projects.find(project => project.route === params.id);
+    if(!project_presents) {
+        return <div>error not found</div>;
+    }
     return (
-        <div>{params.id}</div>
+        <>
+            <div className="row"></div>
+        </>
     )
 }
