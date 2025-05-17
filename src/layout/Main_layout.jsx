232921d25eea924f0c8d0bcaae4bb2983733f535
Hariprasath148 <janhharigithub@gmail.com>
2025-05-17 12:46:01 +0530

    email

--- src/layout/Main_layout.jsx
+++ src/layout/Main_layout.jsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../style/index.css"
 import { Navbar } from '../components/Navbar';
+import { Toaster } from "react-hot-toast";
 
 export const Main_layout = () => {
 
@@ -12,9 +13,10 @@ export const Main_layout = () => {
             <div className='root-container overflow-auto w-100 h-100 rounded-4'>
                 <>
                   <Navbar/>
-                    <div className='main-outlet-container p-3'>
-                      <Outlet/>
-                    </div>
+                  <div className='main-outlet-container p-3'>
+                    <Outlet/>
+                  </div>
+                  <Toaster position="top-right" reverseOrder={false} />
                 </>
             </div>
         </div>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { IoMdMailOpen } from "react-icons/io";
 import { FaPhoneAlt } from "react-icons/fa";
 import { IoLocation } from "react-icons/io5";
 import { MdContactPage } from "react-icons/md";
 import "../style/contact.css"
+import emailjs from "@emailjs/browser";
+import { toast } from 'react-hot-toast';
 
 export const Contact = () => {
+  const form = useRef();
+  const sendEmail = (e) => {
+    console.log("gHello");
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_2jvnuap",     // Replace with your EmailJS service ID
+        "template_pdjft8g",    // Replace with your EmailJS template ID
+        form.current,
+        "dgbEuOSaVOKm1E2nl"      // Replace with your EmailJS public key
+      )
+      .then(
+        (result) => {
+          console.log("Email sent successfully:", result.text);
+          toast.success("Email Received");
+          document.querySelector("#contactForm").reset();
+        },
+        (error) => {
+          console.error("Email send error:", error.text);
+          toast.error("Something went wrong..")
+        }
+      );
+  }
   return (
     <div className='contact-container'>
       <div className="row">
@@ -49,25 +74,25 @@ export const Contact = () => {
         </div>
         <div className='col-lg-6 col-12 p-lg-5 pt-lg-4 p-2 contact-details-container'>
             <div className="form-container bg-white p-4 rounded-4">
-              <form action="#">
+              <form ref={form} id='contactForm' onSubmit={sendEmail}>
                 <div className="form-group">
                   <label htmlFor="name">Name</label>
-                  <input type="text"  className='form-control mt-2 p-2' id='name' aria-describedby='namehelp'/>
+                  <input type="text" required name='name' className='form-control mt-2 p-2' id='name' aria-describedby='namehelp'/>
                 </div>
                 <div className="form-group mt-3">
                   <label htmlFor="email">Email Address</label>
-                  <input type="email"  className='form-control mt-2 p-2' id='email' aria-describedby='emailhelp'/>
+                  <input type="email" required name='email' className='form-control mt-2 p-2' id='email' aria-describedby='emailhelp'/>
                 </div>
                 <div className="form-group mt-3">
                   <label htmlFor="subject">Subject</label>
-                  <input type="text"  className='form-control mt-2 p-2' id='subject' aria-describedby='subjecthelp'/>
+                  <input type="text" required name='subject' className='form-control mt-2 p-2' id='subject' aria-describedby='subjecthelp'/>
                 </div>
                 <div className="form-group mt-3">
-                  <label htmlFor="message">Subject</label>
-                  <textarea className='form-control mt-2 p-2' id='message' rows="3" aria-describedby='subjecthelp'/>
+                  <label htmlFor="message">Message</label>
+                  <textarea name='message' required className='form-control mt-2 p-2' id='message' rows="3" aria-describedby='subjecthelp'/>
                 </div>
                 <div className="form-group mt-4">
-                  <button className='w-100 border-0 submit-buton rounded-pill py-3 mt-2 text-white' id='message' rows="6" aria-describedby='subjecthelp'>Submit</button>
+                  <button type='submit'  className='w-100 border-0 submit-buton rounded-pill py-3 mt-2 text-white' id='message' aria-describedby='subjecthelp'>Submit</button>
                 </div>
               </form>
             </div>
